c09db14bd053143db68e4654bd399a7b63e29b43
gokhan <gokhan@sylow.net>
2011-12-05 22:47:41 +0100

    dynamic product and offer listing

--- config/initializers/sample_data.rb
+++ config/initializers/sample_data.rb
@@ -1,30 +1,45 @@
-products =[
-            {:id => 1,
-             :name => "Product 1",
-             :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-             :image => "90x90.gif"
-            },
-            { :id          => 2,
-              :name        => "Product 1",
-              :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-              :image => "90x90.gif"
-            },
-            { :id          => 3,
-              :name        => "Product 1",
-              :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-              :image => "90x90.gif"
-            },
-            { :id          => 4,
-              :name        => "Product 1",
-              :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-              :image => "90x90.gif"
-            },
-            { :id          => 1,
-              :name        => "Product 1",
-              :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-              :image       => "90x90.gif"
-            }
-]
-offers = [
+module Sample
+  PRODUCTS =[
+              {:id => 1,
+               :name => "Product 1",
+               :description => "Ipsom is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+               :image => "90x90.gif"
+              },
+              { :id          => 2,
+                :name        => "Product 2",
+                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                :image => "90x90.gif"
+              },
+              { :id          => 3,
+                :name        => "Product 3",
+                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                :image => "90x90.gif"
+              },
+              { :id          => 4,
+                :name        => "Product 4",
+                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                :image => "90x90.gif"
+              },
+              { :id          => 5,
+                :name        => "Product 5",
+                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                :image       => "90x90.gif"
+              }
+  ]
+  OFFERS = [
+              { :id=>1, :price=>14.95, :conditions => ["Guarantee 3 years", "Free shipping"], :vendor => "Vendor 1",
+                :description=>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
+              },
+              { :id=>2, :price=>15.95, :conditions => ["Guarantee 3 years", "Free shipping"], :vendor => "Vendor 2",
+                :description=>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
+              },
+              { :id=>3, :price=>16.95, :conditions => ["Guarantee 3 years", "Free shipping"], :vendor => "Vendor 3",
+                :description=>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
+              },
+              { :id=>4, :price=>17.00, :conditions => ["Guarantee 3 years", "Free shipping"], :vendor => "Vendor 4",
+                :description=>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's"
+              }
+                
 
-          ]
+            ]
+end
