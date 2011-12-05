700bd649309ebf763247da81b9bdf866053587b6
gokhan <gokhan@sylow.net>
2011-12-05 23:09:56 +0100

    fotos are added for products

--- config/initializers/sample_data.rb
+++ config/initializers/sample_data.rb
@@ -1,29 +1,24 @@
 module Sample
   PRODUCTS =[
               {:id => 1,
-               :name => "Product 1",
-               :description => "Ipsom is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-               :image => "90x90.gif"
+               :name => "D90",
+               :description=> "Newly designed Nikon DX-format CMOS image sensor with 12.3 effective megapixels and Integrated Dust Reduction System. Incredibly low-noise performance throughout a wide sensitivity range of ISO 200 to 3200; can be set to ISO 6400 equivalent",
+               :image => "/products/product_d90.png"
               },
               { :id          => 2,
-                :name        => "Product 2",
-                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                :image => "90x90.gif"
+                :name        => "D3000",
+                :description => "New Guide mode provides in-camera assistance for making camera settings and utilizing professional techniques. Large 3.0-in., approx. 230k-dot LCD with brightness adjustment. Nikon DX-format CCD image sensor, 10.2 effective megapixels. Integrated Dust Reduction System. Nikon's exclusive EXPEED image processing system.",
+                :image => "/products/product_d3000.png"
               },
               { :id          => 3,
-                :name        => "Product 3",
-                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                :image => "90x90.gif"
+                :name        => "D31000",
+                :description => "D3100 employs Nikon's latest breakthrough technology to deliver superb results. A newly developed DX-format CMOS sensor with 14.2 effective megapixels works in tandem with new EXPEED 2 image processing engine to produce highly detailed images containing rich tonal gradations and faithful color.",
+                :image => "/products/product_d3100.png"
               },
               { :id          => 4,
-                :name        => "Product 4",
-                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                :image => "90x90.gif"
-              },
-              { :id          => 5,
-                :name        => "Product 5",
-                :description => "Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-                :image       => "90x90.gif"
+                :name        => "D5100",
+                :description => "The D5100 employs an image sensor equivalent to that of the higher-class model, D7000. Furthermore, image processing engine 'EXPEED 2' achieves high image quality, high-speed image processing and multiple functions.",
+                :image => "/products/product_d5100.png"
               }
   ]
   OFFERS = [
